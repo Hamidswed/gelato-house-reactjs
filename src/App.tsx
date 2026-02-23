6be4769b86e1f6741ed605f8a0c8d705768b825f
Hamidreza <delshad.swdn@gmail.com>
2026-02-23 10:46:22 +0330

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <div className="bg-amber-500 font-bold text-white text-2xl">
+      Hello world
+    </div>
+  );
 }
 
 export default App;
